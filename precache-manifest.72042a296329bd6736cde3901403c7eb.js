0c657ffddc6ba9c437deb508387701fa71882f61
Hanna <hanna-zalami@gmx.de>
2020-06-25 12:33:09 +0200

    Updates

---
+++ precache-manifest.72042a296329bd6736cde3901403c7eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1246db753d64fcbc6ebfec8784437995",
+    "url": "/web-nfc-scan-image-app/index.html"
+  },
+  {
+    "revision": "661d32034732b9a35766",
+    "url": "/web-nfc-scan-image-app/static/css/main.aacd008b.chunk.css"
+  },
+  {
+    "revision": "b7d6b55c43f819244b63",
+    "url": "/web-nfc-scan-image-app/static/js/2.27df537a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/web-nfc-scan-image-app/static/js/2.27df537a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "661d32034732b9a35766",
+    "url": "/web-nfc-scan-image-app/static/js/main.9abbe681.chunk.js"
+  },
+  {
+    "revision": "25c223e199d89fb52d9c",
+    "url": "/web-nfc-scan-image-app/static/js/runtime-main.1afa46ec.js"
+  }
+]);
